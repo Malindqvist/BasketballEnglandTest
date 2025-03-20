3a1ed7c67ac137ba7f9d31ee0e90dfccefca1a36
malindqvist <malindqvist@outlook.com>
2025-03-20 21:46:11 +0100

    Simplified all scenarios. Split valid and invalid into two scenarios. Moved logic to a new StepHelper-class.

--- src/test/java/stepDefinitions/stepDefinitions.java
+++ src/test/java/stepDefinitions/stepDefinitions.java
@@ -1,5 +1,6 @@
 package stepDefinitions;
 
+import general.StepHelper;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -7,17 +8,20 @@ import io.cucumber.java.en.When;
 import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
+import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class stepDefinitions {
     private WebDriver driver;
+    WebDriverWait wait;
+    StepHelper stepHelper;
 
     @After
     public void tearDown() {
@@ -36,138 +40,65 @@ public class stepDefinitions {
     @Given("the user is on the account creation page")
     public void userNavigatesToAccountCreationPage() {
         driver.get("https://membership.basketballengland.co.uk/NewSupporterAccount");
-        String expected = "https://membership.basketballengland.co.uk/NewSupporterAccount";
-        String actual = driver.getCurrentUrl();
-
-        assertEquals(expected, actual);
     }
 
-    @And("the user enters {string} in the Date of Birth field")
-    public void userEntersDateOfBirth(String dateOfBirth) {
-        driver.findElement(By.cssSelector("#dp")).sendKeys(dateOfBirth);
-
-    }
-
-    @And("the user enters {string} in the First Name field")
-    public void userEntersFirstName(String expected) {
-        WebElement firstname = driver.findElement(By.name("Forename"));
-        firstname.sendKeys(expected);
-        String actual = firstname.getDomProperty("value");
-        assertEquals(expected, actual);
-    }
-
-    @And("the user enters {string} in the Last Name field")
-    public void userEntersLastName(String expected) {
-        WebElement lastname = driver.findElement(By.name("Surname"));
-        lastname.sendKeys(expected);
-        String actual = lastname.getDomProperty("value");
-        assertEquals(expected, actual);
-    }
-
-    @And("the user enters {string} in the Email fields")
-    public void theUserEntersEmail(String email) {
-        WebElement emailAdress = driver.findElement(By.cssSelector("#member_emailaddress"));
-        WebElement confirmEmailAdress = driver.findElement(By.cssSelector("#member_confirmemailaddress"));
-        emailAdress.sendKeys(email);
-        confirmEmailAdress.sendKeys(email);
-    }
-
-    @And("the user enters {string} and {string} in the Password Fields")
-    public void theUserEntersPassword(String password, String password2) {
-        driver.findElement(By.cssSelector("#signupunlicenced_password")).sendKeys(password);
-        driver.findElement(By.cssSelector("#signupunlicenced_confirmpassword")).sendKeys(password2);
-    }
-
-    @And("the user selects their {string} in the Role form")
-    public void theUserSelectsTheirRole(String role) {
-        //Creates an array with each role separated
-        String[] roles = role.split(", ");
-        List<WebElement> supporterRoles = driver.findElements(By.cssSelector("label[for^='signup_basketballrole']"));
-
-        for (String s : roles) {
-            for (WebElement supporterRole : supporterRoles) {
-                if (supporterRole.getText().equals(s)) {
-                    supporterRole.click();
-                }
-
-            }
+    @And("the user enters {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}")
+    public void theUserEntersInput(String dateOfBirth, String firstName, String lastName, String email, String password, String passwordConfirmation,String role, String comPrefs, String choice) {
+        stepHelper = new StepHelper(driver);
+        stepHelper.enterText(By.cssSelector("#dp"), dateOfBirth);
+        stepHelper.enterText(By.name("Forename"), firstName);
+        stepHelper.enterText(By.name("Surname"), lastName);
+        stepHelper.enterText(By.cssSelector("#member_emailaddress"), email);
+        stepHelper.enterText(By.cssSelector("#member_confirmemailaddress"), email);
+        stepHelper.enterText(By.cssSelector("#signupunlicenced_password"), password);
+        stepHelper.enterText(By.cssSelector("#signupunlicenced_confirmpassword"), passwordConfirmation);
+        stepHelper.clickElements(By.cssSelector("label[for^='signup_basketballrole']"), role);
+        stepHelper.clickElement(By.cssSelector("label[for='sign_up_25"));
+        stepHelper.clickElement(By.cssSelector("label[for='sign_up_26"));
+        stepHelper.clickComPrefs(comPrefs);
+        if(choice.equals("Accept")) {
+            stepHelper.clickElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct']"));
         }
-
-    }
-
-    @And("the user accepts the Terms and Conditions")
-    public void theUserAcceptsTheTermsAndConditions() {
-        driver.findElement(By.cssSelector("label[for='sign_up_25']")).click();
-    }
-
-    @And("the user confirms age over 18")
-    public void theUserConfirmsAgeOver18() {
-        driver.findElement(By.cssSelector("label[for='sign_up_26']")).click();
-    }
-
-    @And("the user checks their communication preferences {string}")
-    public void theUserChecksTheir(String preferences) {
-        WebElement marketingCheckbox = driver.findElement(By.cssSelector("label[for='sign_up_27']"));
-        WebElement partnersCheckbox = driver.findElement(By.cssSelector("label[for='sign_up_28']"));
-
-        switch (preferences) {
-            case "Marketing":
-                marketingCheckbox.click();
-                break;
-            case "Partners":
-                partnersCheckbox.click();
-                break;
-            case "Marketing & Partners":
-                marketingCheckbox.click();
-                partnersCheckbox.click();
-                break;
-        }
-    }
-
-
-    @And("the user {string} the Code of Ethics and Conduct")
-    public void theUserAcceptsTheCodeOfEthicsAndConduct(String choice) {
-
-        if(choice.equals("Accept")){
-            driver.findElement(By.cssSelector("label[for='fanmembersignup_agreetocodeofethicsandconduct']")).click();
-        }
-
     }
 
     @When("the user clicks on the Confirm and Join-button")
     public void theUserClicksOnTheConfirmAndJoinButton() {
-        driver.findElement(By.cssSelector("input[name='join']")).click();
+        stepHelper.clickElement(By.cssSelector("input[name='join']"));
     }
 
-    @Then("{string} should occur")
-    public void expectedOutcome(String expectedOutcome) {
-        System.out.println(expectedOutcome);
-
+    @Then("the account is created")
+    public void theAccountIsCreated() {
+        String actual = driver.findElement(By.cssSelector("h2")).getText();
+        String expected = "THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND";
+        assertEquals(expected, actual);
     }
 
-    @And("if invalid the user should see the error message {string}")
-    public void ifInvalidTheUserShouldSeeTheErrorMessage(String errorMessage) {
-        System.out.println(errorMessage);
+    @And("the user should see the error message {string}")
+    public void theUserShouldSeeErrorMessage(String errorMessage) {
         switch (errorMessage) {
-            case "Last Name is required":
+            case "Last Name is missing":
                 assertTrue(driver.findElement(By.cssSelector("span[for='member_lastname']")).isDisplayed());
                 break;
-            case "Password did not match":
+            case "Passwords do not match":
                 assertTrue(driver.findElement(By.cssSelector("span[for='signupunlicenced_confirmpassword']")).isDisplayed());
                 break;
-            case "Code of Ethics and Conduct":
+            case "Code of Ethics and Conduct declined":
                 assertTrue(driver.findElement(By.cssSelector("span[for^='AgreeToCodeOfEthicsAndConduct']")).isDisplayed());
                 break;
-
+            case "Parent last name missing":
+                assertTrue(driver.findElement(By.cssSelector("span[data-valmsg-for='ParentSurname']")).isDisplayed());
+                break;
         }
     }
 
-    @Then("the account should be created")
-    public void theAccountShouldBeCreated() {
-//        String actual = driver.findElement(By.cssSelector("h2")).getText();
-//        String expected = "THANK YOU FOR CREATING AN ACCOUNT WITH BASKETBALL ENGLAND";
-//        assertEquals(expected, actual);
-        driver.quit();
-    }
+    @And("the user enters {string}, {string}, {string}")
+    public void theUserEntersParentalInput(String parentFirstName, String parentLastName, String parentEmail) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("ParentForename")));
 
+        stepHelper.enterText(By.name("ParentForename"), parentFirstName);
+        stepHelper.enterText(By.name("ParentSurname"), parentLastName);
+        stepHelper.enterText(By.name("ParentEmailAddress"), parentEmail);
+        stepHelper.enterText(By.name("ParentConfirmEmailAddress"), parentEmail);
+    }
 }
--- src/test/resources/features/BasketballEnglandTest.feature
+++ src/test/resources/features/BasketballEnglandTest.feature
@@ -3,26 +3,38 @@ Feature: Basketball England Account Creation
   Scenario Outline: Create an account
     Given the user is using the browser "<browser>"
     Given the user is on the account creation page
-    And the user enters "<firstName>" in the First Name field
-    * the user enters "<lastName>" in the Last Name field
-    * the user enters "<dateOfBirth>" in the Date of Birth field
-    * the user enters "<email>" in the Email fields
-    * the user enters "<password>" and "<passwordConfirmation>" in the Password Fields
-    * the user selects their "<role>" in the Role form
-    * the user accepts the Terms and Conditions
-    * the user confirms age over 18
-    * the user checks their communication preferences "<communicationPreferences>"
-    * the user "<choice>" the Code of Ethics and Conduct
+    And the user enters "<dateOfBirth>", "<firstName>", "<lastName>", "<email>", "<password>", "<passwordConfirmation>", "<role>", "<comPrefs>", "<choice>"
     When the user clicks on the Confirm and Join-button
-    Then "<expectedOutcome>" should occur
-    And if invalid the user should see the error message "<errorMessage>"
-
+    Then the account is created
 
     Examples:
-      | browser | dateOfBirth | firstName | lastName   | email          | password | passwordConfirmation | role                               | communicationPreferences | expectedOutcome | errorMessage               | choice  |
-      | Chrome  | 01/01/1930  | Lennart   | Lindqvist  | test1@test.com | test123  | test123              | Fan, Coach, Player                 | Marketing                | Success         |                            | Accept  |
-      | Chrome  | 02/12/2000  | Nils      |            | test2@test.com | test123  | test123              | Coach, Sports science role         | Partners                 | Failure         | Last Name is required      | Accept  |
-      | Chrome  | 29/02/2000  | Erik      | Henriksson | test3@test.com | test123  | test234              | Player, Player's relative/guardian | Marketing & Partners     | Failure         | Password did not match     | Accept  |
-      | Chrome  | 29/02/2000  | Jens      | Hansson    | test4@test.com | test123  | test123              | Player                             | Marketing                | Failure         | Code of Ethics and Conduct | Decline |
+      | browser | dateOfBirth | firstName | lastName   | email             | password | passwordConfirmation | role                       | comPrefs             | choice  |
+      | Chrome  | 01/01/1930  | Lennart   | Lindqvist  | t0303@example.com | test123  | test123              | Fan, Coach, Player         | Marketing            | Accept  |
+      | Firefox | 02/12/2000  | Nils      | Östlund    | t0304@example.com | test123  | test123              | Coach, Sports science role | Partners             | Accept  |
 
+  Scenario Outline: Create an account with invalid input
+    Given the user is using the browser "<browser>"
+    Given the user is on the account creation page
+    And the user enters "<dateOfBirth>", "<firstName>", "<lastName>", "<email>", "<password>", "<passwordConfirmation>", "<role>", "<comPrefs>", "<choice>"
+    When the user clicks on the Confirm and Join-button
+    Then the user should see the error message "<errorMessage>"
+
+    Examples:
+      | browser | dateOfBirth | firstName | lastName   | email             | password | passwordConfirmation | role                               | comPrefs             | errorMessage               | choice  |
+      | Chrome  | 02/12/2000  | Nils      |            | t0305@example.com | test123  | test123              | Coach, Sports science role         | Partners             | Last name is missing       | Accept  |
+      | Chrome  | 29/02/2000  | Erik      | Henriksson | t0306@example.com | test123  | test234              | Player, Player's relative/guardian | Marketing & Partners | Passwords do not match     | Accept  |
+      | Chrome  | 29/02/2000  | Jens      | Hansson    | t0307@example.com | test123  | test123              | Player                             | Marketing            | Code of Ethics and Conduct | Decline |
+
+  Scenario Outline: Create an account with parental responsibility
+    Given the user is using the browser "<browser>"
+    Given the user is on the account creation page
+    And the user enters "<dateOfBirth>", "<firstName>", "<lastName>", "<email>", "<password>", "<passwordConfirmation>", "<role>", "<comPrefs>", "<choice>"
+    And the user enters "<parentFirstName>", "<parentLastName>", "<parentEmail>"
+    When the user clicks on the Confirm and Join-button
+    Then the account is created
+
+    Examples:
+      | browser | dateOfBirth | firstName | lastName   | email             | password | passwordConfirmation | role                       | comPrefs  | choice  | parentFirstName | parentLastName | parentEmail       |
+      | Chrome  | 01/01/2012  | Lennart   | Lindqvist  | t0314@example.com | test123  | test123              | Fan, Coach, Player         | Marketing | Accept  | Agda            | Bengtsson      | t0312@example.com |
+      | Firefox | 02/12/2012  | Nils      | Östlund    | t0315@example.com | test123  | test123              | Coach, Sports science role | Partners  | Accept  | Henrik          | Nilsson        | t0313@example.com |
 
